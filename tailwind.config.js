c3871eab45933c55e52bb37aeb4f4f16e4075b68
Ian.arkhipov <85026167+interiorengine@users.noreply.github.com>
2024-01-11 17:37:06 +0300

    Project performed, edits

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ["./index.html"],
+  // darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateRows: {
+        'layout': 'auto 1fr auto'
+      },
+      colors: {
+      //  your custom colors
+      }
+      fontFamily: {
+        main: ['Montserrat'],
+      },
+    },
   },
   plugins: [],
 }
