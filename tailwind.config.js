4f53ddc2e1b9f77bc5bec039bc8b349317128595
Ian.arkhipov <85026167+interiorengine@users.noreply.github.com>
2024-01-24 17:13:14 +0300

    dist/styles.css is built

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,19 @@
 module.exports = {
   content: ["./index.html"],
   // darkMode: 'class',
+  purge: [
+    './index.html',
+    './tailwind.config.js',
+    './src/main.css',
+  ],
   theme: {
     extend: {
       gridTemplateRows: {
         'layout': 'auto 1fr auto'
       },
       colors: {
-      //  your custom colors
-      }
+        //  your custom colors
+      },
       fontFamily: {
         main: ['Montserrat'],
       },
